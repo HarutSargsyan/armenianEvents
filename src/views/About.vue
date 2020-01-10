cb72828e5292e782da53f4c834773d66cb9ce668
Harut <harutyun.s4.y@tumo.org>
2020-01-10 17:03:27 +0400

    finish

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <h1 style="text-align:center" class="font-italic font-weight-medium mb-4 black--text">Our Mission</h1>
+      <h1 class="text-center font-italic font-weight-medium mb-4 black--text">Our Mission</h1>
     <p class="mt-10 text-justify">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium hic, laborum ducimus totam dolorem voluptatem magni rerum labore provident, consequuntur ipsa nesciunt ab quas quae expedita! Ad doloribus eius consectetur.
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut incidunt voluptatum possimus mollitia modi nesciunt omnis, voluptas facilis in? Temporibus necessitatibus inventore iure possimus, beatae aut est ducimus molestiae minus!
@@ -11,12 +11,12 @@
       <v-row align="center" justify="center"></v-row>
     </v-parallax>
 
-    <h2 class="mt-12 mb-6 display-1 text-center">Meet our team</h2>
+    <h2 class="mt-12 mb-6 display-1 text-center font-italic font-weight-medium mb-4 black--text">Meet our team</h2>
     <v-row align="center" justify="center">
       <v-col cols="sm-and-up-4 xs-12">
         <v-hover v-slot:default="{ hover }">
           <v-card dark color="purple darken-4" :elevation="hover ? 12 : 2" class="mx-auto" max-width="350">
-            <v-img class="white--text align-end" height="200px" src="https://scontent.fevn1-4.fna.fbcdn.net/v/t1.0-9/80647115_977394329301631_9000118459987656704_n.jpg?_nc_cat=102&_nc_ohc=QgGvWBJJ_6wAQkY6AdfpG6UVWn86Cwb2MExaH9W109HhOGRvdeAxjyevg&_nc_ht=scontent.fevn1-4.fna&oh=8b53a29a24e8993af8f1d8001a315774&oe=5EDBAD74">
+            <v-img  height="200px" src="https://scontent.fevn1-4.fna.fbcdn.net/v/t1.0-9/80647115_977394329301631_9000118459987656704_n.jpg?_nc_cat=102&_nc_ohc=QgGvWBJJ_6wAQkY6AdfpG6UVWn86Cwb2MExaH9W109HhOGRvdeAxjyevg&_nc_ht=scontent.fevn1-4.fna&oh=8b53a29a24e8993af8f1d8001a315774&oe=5EDBAD74">
               <v-card-title>Harut Sargsyan</v-card-title>
             </v-img>
 
@@ -31,34 +31,35 @@
       </v-col>
       <v-col cols="sm-and-up-4 xs-12">
         <v-hover v-slot:default="{ hover }">
-          <v-card :elevation="hover ? 12 : 2" class="mx-auto" max-width="350">
-            <v-img class="white--text align-end" height="200px" src="arman.jpg">
+          <v-card  dark color="purple darken-4" :elevation="hover ? 12 : 2" class="mx-auto" max-width="350">
+            <v-img height="200px" src="../../public/arman.jpg">
               <v-card-title>Arman Khandanyan</v-card-title>
             </v-img>
 
             <v-card-subtitle class="pb-0">Back-end developer</v-card-subtitle>
 
-            <v-card-text class="text--primary">
-              <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil ipsum architecto exercitationem quisquam commodi quis quasi perspiciatis amet voluptatum unde maxime incidunt, eum inventore voluptatibus aut repellendus nemo deserunt modi!</div>
+            <v-card-text class="white--text">
+              <div>Hi I'm Arman and i'm 13. Although that I participated to few workshops in TUMO I get so interseting 
+                and usefull knowledge about Javascript. I like Js so much, and I built this project with one of best JS frameworks Vue.js. I attend TUMO 1 year. </div>
             </v-card-text>
           </v-card>
         </v-hover>
       </v-col>
       <v-col cols="sm-and-up-4 xs-12">
         <v-hover v-slot:default="{ hover }">
-          <v-card :elevation="hover ? 12 : 2" class="mx-auto" max-width="350">
+          <v-card dark color="purple darken-4"  :elevation="hover ? 12 : 2" class="mx-auto" max-width="350">
             <v-img
-              class="white--text align-end"
               height="200px"
-              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+             src="../../public/Karine.jpg"
             >
               <v-card-title>Karine Ter-Minasyan</v-card-title>
             </v-img>
 
             <v-card-subtitle class="pb-0">Front-end developer</v-card-subtitle>
 
-            <v-card-text class="text--primary">
-              <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil ipsum architecto exercitationem quisquam commodi quis quasi perspiciatis amet voluptatum unde maxime incidunt, eum inventore voluptatibus aut repellendus nemo deserunt modi!</div>
+            <v-card-text class=" white--text ">
+              <div> Hi I'm Karine and i'm 20. I participated so many workshops in TUMO get so interseting 
+                and usefull knowledge about Robotics, Graphic design and of course Javascript. I like Js so much, and I built this project with one of best JS frameworks Vue.js. I'm TUMO alumni !!!</div>
             </v-card-text>
           </v-card>
         </v-hover>
--- src/views/Display.vue
+++ src/views/Display.vue
@@ -15,79 +15,115 @@
             <h3>Event by: {{ eventInfo.host }}</h3>
           </v-col>
         </v-row>
-        <v-divider/>
+        <v-divider />
         <v-row class="text-center" justify="space-around">
           <v-col cols="10" md="2">
             <v-icon>mdi-clock</v-icon>
-            <br>
+            <br />
             <div>{{ formatTime(eventInfo.time) }}</div>
           </v-col>
           <v-col cols="10" md="2">
             <v-icon>mdi-account</v-icon>
-            <br>
-            <div>Guests: {{ eventInfo.guests }}</div>
+            <br />
+            <div>Guests: {{ guests }}</div>
           </v-col>
           <v-col cols="10" md="2">
             <v-icon>mdi-map-marker</v-icon>
-            <br>
+            <br />
             <div>Located at: {{ eventInfo.position }}</div>
           </v-col>
         </v-row>
-        <v-divider/>
+        <v-divider />
         <v-row justify="center">
           <v-col cols="12">
-            <h1>About this Event:</h1>
-            <p>{{ eventInfo.description }}</p>
+            <h1 class="font-weight-light" style="margin-bottom:10px">About this Event:</h1>
+            <p class="display-1">{{ eventInfo.description }}</p>
           </v-col>
-          <v-divider/>
+          <v-divider />
           <v-col cols="12" md="5">
-            <v-img
-            :src="eventInfo.img"
-            aspect-ratio="1"
-            max-height="300">
-            </v-img>
+            <v-img :src="eventInfo.img" aspect-ratio="1" max-height="300"></v-img>
           </v-col>
         </v-row>
       </v-card-text>
-      <v-card-actions>
-        <v-btn>Check in</v-btn>
-      </v-card-actions>
+      <div class="text-center">
+        <v-btn
+          :disabled="dialog || clicked"
+          :loading="dialog"
+          class="white--text"
+          color="purple darken-4"
+          @click="dialog = true; clicked = true; checkIn()"
+        >Check In</v-btn>
+        <v-dialog v-model="dialog" hide-overlay persistent width="300">
+          <v-card color="purple darken-4" dark>
+            <v-card-text>
+              Checking In...
+              <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+      </div>
     </v-card>
   </div>
 </template>
 
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   name: "About",
   data: () => ({
+    dialog: false,
     eventInfo: null,
-    loading: true
+    guests: null,
+    loading: true,
+    clicked: false
   }),
-  methods: {
-    formatTime: function (date) {
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    var strTime = hours + ':' + minutes + ' ' + ampm;
-    return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+  watch: {
+      dialog (val) {
+        if (!val) return
+
+        setTimeout(() => (this.dialog = false), 4000)
+      },
     },
-    getEventInfo: async function () {
+  methods: {
+    checkIn: function () {
+      this.guests++
+    },
+    formatTime: function(date) {
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var ampm = hours >= 12 ? "pm" : "am";
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      var strTime = hours + ":" + minutes + " " + ampm;
+      return (
+        date.getMonth() +
+        1 +
+        "/" +
+        date.getDate() +
+        "/" +
+        date.getFullYear() +
+        "  " +
+        strTime
+      );
+    },
+    getEventInfo: async function() {
       const params = {
         id: this.$route.params.id
-      }
-      let response = await axios.get('https://armenian-events.herokuapp.com/getEventInfo/', { params })
-      this.eventInfo = response.data['eventInfo']
-      this.eventInfo.time = new Date(this.eventInfo.time)
-      this.loading = false
+      };
+      let response = await axios.get(
+        "https://armenian-events.herokuapp.com/getEventInfo/",
+        { params }
+      );
+      this.eventInfo = response.data["eventInfo"];
+      this.eventInfo.time = new Date(this.eventInfo.time);
+      this.guests = this.eventInfo.guests
+      this.loading = false;
     }
   },
-  created () {
-    this.getEventInfo()
+  created() {
+    this.getEventInfo();
   }
 };
 </script>
